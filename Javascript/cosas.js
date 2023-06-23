0cf34b3025d420203406a34f1b03c037037d6ba9
sebasmalonce_github <antoniomaldonadoestudio@gmail.com>
2023-06-23 12:24:31 -0500

    finish

--- Javascript/cosas.js
+++ Javascript/cosas.js
@@ -1,7 +1,22 @@
-function validación() {
-    let promtEdad = prompt("Para darte tu licencia de conducir, necesito saber: cuantos años tienes?");
-    let edad = parseInt(promtEdad);
-    let confirmar = edad >= 18 && edad <= 49 ? "Puedes conducir" : "No puedes conducir" 
-    alert(confirmar)
-};
-validación();
+const piedra = "Piedra " + '\u{1FAA8}';
+const papel = "Papel " + '\u{1F4C4}';
+const tijera = "Tijera " + '\u{2702}';
+
+function game (jugador, pc){
+    const mensaje = "Estas son tus opciones: " + piedra + "  " + papel + "  " + tijera;
+    const advertencia = "RECUERDA: Solamente puedes escribir la palabra, NO EL EMOJI."
+    const comienzaGame = "Comienza el juego"
+    
+    alert(mensaje + advertencia);
+    alert(comienzaGame)
+    
+    jugador = prompt("Tú eliges: ")
+    pc = alert("pc eligió " + piedra)
+    if (jugador === piedra && pc === piedra) {
+        alert("ganaste")
+    } else {
+        alert("Perdiste")
+    }
+}
+
+game();
